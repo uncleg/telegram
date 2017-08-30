9471598f0d93b9f6edae35ad424ee764e8a6155f
Your Name <you@example.com>
2017-08-29 21:58:22 -0300

    update

--- sendpicture2Telegram.py
+++ sendpicture2Telegram.py
@@ -10,35 +10,41 @@ import ConfigParser
 config = ConfigParser.ConfigParser()
 config.read("../sensorCAM.conf")
 token=config.get('Telegram','token')
-chat_id=config.getint('Telegram','chat_id')
+chat_id=config.get('Telegram','chat_id').split(',')
+sensorCAM = config.get('Telegram','sensorCAM')
 
 
 #setting other variables
 global p
 
 #@authenticate
-'''
-def start(bot, update):
-	if update.message.chat_id == chat_id: 
-		update.message.reply_text('Welcome')
+
+def help(bot, update):
+	if str(update.message.chat_id) in chat_id: 
+		update.message.reply_text('Tienes disponibles los siguientes comandos:')
+		update.message.reply_text('/up  Inicia el sensorCAM')
+		update.message.reply_text('/down  Apaga el sensorCAM')
+		update.message.reply_text('/status  Muestra el status del sensorCAM')
 	else:
 		update.message.reply_text('Sorry. Action not allowed for you')
     	print "Access not allowed"+update.message.chat_id
-'''
-def start(bot, update):
-	if update.message.chat_id == chat_id: 
+
+def up_sensor(bot, update):
+	if str(update.message.chat_id) in chat_id: 
 		update.message.reply_text('Starting sensorCAM...')
 		#p = subprocess.Popen(["python","/home/pi/Arduino/just-sensoring-telegram.py"],stdout=subprocess.PIPE)
 		global p
 		p = subprocess.Popen(["python","printing.py"],stdout=subprocess.PIPE)
+		#p = subprocess.Popen(sensorCAM,stdout=subprocess.PIPE)
 		update.message.reply_text(p.pid)
 	else:
 		update.message.reply_text('Sorry. Action not allowed for you')
     	print "Access not allowed"+update.message.chat_id
 
-def stop(bot, update):
+def down_sensor(bot, update):
 	global p
-	if update.message.chat_id == chat_id: 
+	print sensorCAM
+	if str(update.message.chat_id) in chat_id: 
 		update.message.reply_text('Stopping sensorCAM...')
 		#p.kill()
 		p.terminate()
@@ -48,7 +54,7 @@ def stop(bot, update):
 
 def status(bot, update):
 	global p
-	if update.message.chat_id == chat_id: 
+	if str(update.message.chat_id) in chat_id: 
 		update.message.reply_text('sensorCAM Status:')
 		if p.poll() == None:
 			update.message.reply_text('sensorCAM Alive with PID:')
@@ -60,37 +66,31 @@ def status(bot, update):
     	print "Access not allowed"+update.message.chat_id
 
 #@authenticate
-def hello(bot, update):
-    if update.message.chat_id == chat_id:
+def start(bot, update):
+    if str(update.message.chat_id) in chat_id:
    		update.message.reply_text(
-        'Hello {}'.format(update.message.from_user.first_name))
+        'Hello {}. Welcome to BotAlarma!'.format(update.message.from_user.first_name))
     else: 
     	update.message.reply_text('Sorry. Action not allowed for you')
-    	print "Access not allowed"+update.message.chat_id
-    	print update.users.userFull(update.message.chat_id)
+    	print type(update.message.chat_id)
 
 #@authenticate
-def capture(bot, update):
-    cantidad=5
-    if update.message.chat_id == chat_id:
-		stop()
-		status()
-		start()	
+def capture(bot, update, args):
+    if str(update.message.chat_id) in chat_id:
+			
     else:
 		update.message.reply_text('Sorry. Action not allowed for you')
 		print "Access not allowed"+update.message.chat_id
     
 #camera = picamera.PiCamera()
 
+#print sensorCAM
 updater = Updater(token)
-#bot = telegram.Bot(token='415654247:AAExstPW8p9OBka7Pt8RcTIxkZt0pug-CSQ')
-
-
+updater.dispatcher.add_handler(CommandHandler('?', help))
 updater.dispatcher.add_handler(CommandHandler('start', start))
-#updater.dispatcher.add_handler(CommandHandler('inicio', inicio))
-updater.dispatcher.add_handler(CommandHandler('stop', stop))
+updater.dispatcher.add_handler(CommandHandler('up', up_sensor))
+updater.dispatcher.add_handler(CommandHandler('down', down_sensor))
 updater.dispatcher.add_handler(CommandHandler('status', status))
-updater.dispatcher.add_handler(CommandHandler('hello', hello))
 updater.dispatcher.add_handler(CommandHandler('capture', capture))
 
 updater.start_polling()
